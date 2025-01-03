9361ec35d4cab808b3abeb5a093709833382a3cc
Maxwell2345 <maxwell@pedersenfam.com>
2025-01-03 13:20:01 -0700

    Optimization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FieldCentricRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FieldCentricRed.java
@@ -12,10 +12,7 @@ public class FieldCentricRed extends FieldCentricUtil {
 
         hardwareMaps();
 
-        initializeMotors();
-        initializeServos();
-        servo_power_sets();
-        set_up_imu();
+        Initialize();
         waitForStart();
         if (opModeIsActive()) {
             // Put run blocks here.
@@ -23,14 +20,13 @@ public class FieldCentricRed extends FieldCentricUtil {
             servo_power_sets();
             initializeLights();
             while (opModeIsActive()) {
-                rotate_x_and_y(gamepad1.left_stick_y, gamepad1.left_stick_x);
-                drive_control(gamepad1.right_stick_x);
+                FieldCentric(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.right_bumper);
                 assent_control(gamepad2.left_bumper, gamepad2.right_bumper);
-                slide_control(gamepad2.right_stick_y, -gamepad2.left_stick_y, gamepad2.touchpad, 0.2);
+                slide_control(gamepad2.right_stick_y, -gamepad2.left_stick_y, 0.1);
                 red_intake_control(gamepad2.right_trigger, gamepad2.left_trigger, gamepad2.dpad_left, gamepad2.dpad_right, gamepad2.touchpad, 0.1);
                 sample_control(gamepad2.y, gamepad2.a);
                 specimen_control(gamepad2.x, gamepad2.b);
-                power_sets(gamepad1.right_bumper);
+                power_sets();
                 doIntake();
                 telemetry();
             }
