08e1c89370fd3dad66139217f5944380c103f8b6
starwarsfish <141283535+starwarsfish@users.noreply.github.com>
2025-01-14 13:19:31 -0700

    attempt to tune transfer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/TeleOp/FieldCentricUtil.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/TeleOp/FieldCentricUtil.java
@@ -603,9 +603,9 @@ public class FieldCentricUtil extends LinearOpMode {
                 sampleServo.setPosition(0.65);
                 intakePivot.setPosition(0.73);
                 TimeUnit.MILLISECONDS.sleep(500);
-                left.setPower(-0.1);
-                right.setPower(-0.1);
-                TimeUnit.MILLISECONDS.sleep(400);
+                left.setPower(-0.05);
+                right.setPower(-0.05);
+                TimeUnit.MILLISECONDS.sleep(100);
                 sampleServo.setPosition(0.71);
                 left.setPower(0);
                 right.setPower(0);
